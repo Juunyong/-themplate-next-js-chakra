00d4aa2c726212ed4afd0fc9f476a494d45da3e8
Juunyong <qkrwnsdyd212@gmail.com>
2024-11-28 10:50:38 +0900

    first commit

---
+++ src/app/home/home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import IntroSwiper from './intro'
+import News from './news'
+
+const Home = () => {
+  return (
+    <div>
+      {/* swiper */}
+      <IntroSwiper />
+      {/* news */}
+      <News />
+      {/* stock */}
+      
+    </div>
+  )
+}
+
+export default Home
